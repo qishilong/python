12b63cf76a76c626647fc8399b56eec7d3e87cd2
qishilong <663253062@qq.com>
2025-12-09 23:12:44 +0800

    feat(入门篇): 运算符.py

---
+++ 入门篇/运算符.py
@@ -0,0 +1,14 @@
+a = 1 + 1
+b = 2 - 1
+c = 2 * 3
+d = 1 / 3  # 普通除法，结果是浮点数
+e = 7 // 3  # 取整除，结果是整数
+f = 7 % 3
+g = 2**3
+print(a)
+print(b)
+print(c)
+print(d, "除法", type(d))
+print(e, "取整除", type(e))
+print(f)
+print(g)
