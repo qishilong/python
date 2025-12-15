b4d29014eae0d47f0b2e4612b55ac668402ef696
qishilong <663253062@qq.com>
2025-12-15 21:49:17 +0800

    feat(入门篇): 字符串.py

---
+++ 入门篇/数据输入.py
@@ -0,0 +1,11 @@
+# print("你是谁？")
+# name = input()
+# print("我知道了，你是：%s" % name)
+
+# name = input("你是谁？")
+# print("我知道了，你是：%s" % name)
+
+# 键盘输入的数据默认是字符串类型
+val = input("请输入一个数：")
+val = int(val)
+print("数字类型是%s" % type(val))
